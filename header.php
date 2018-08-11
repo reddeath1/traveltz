46e85d2c4eb05e891c1558e57dcb470044db8dad
brysonkimario <bryson.john0@gmail.com>
2018-08-11 14:24:58 +0300

    Setting up the Home Page

--- header.php
+++ header.php
@@ -2,27 +2,34 @@
 include_once('head.php');
 ?>
 
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <h1>My First Bootstrap 4 Page</h1>
-  <p>Resize this responsive page to see the effect!</p>
-</div>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+
+<!--Navigation Bar Starts Here-->
+<nav class="navbar navbar-expand-sm">
+
+    <!-- Logo goes here -->
+    <a class="navbar-brand" href="#">LOGO HERE
+  </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-  </div>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+
+        <!-- Menu Items -->
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
 </nav>
+
+
+
+<!--The Banner starts here-->
