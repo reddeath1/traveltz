1d3d5fad60f60c7a92b7fee0ee8aec54ca82137f
kaykhahima <kaykhahima@gmail.com>
2018-08-15 20:09:42 +0300

    added a link

--- header.php
+++ header.php
@@ -7,25 +7,28 @@ include_once('head.php');
 <!--Navigation Bar Starts Here-->
 <nav class="navbar navbar-expand-sm">
 
-    <!-- Logo goes here -->
-    <a class="navbar-brand" href="#">LOGO HERE
+	<!-- Logo goes here -->
+	<a class="navbar-brand" href="#">LOGO HERE
   </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+	<div class="collapse navbar-collapse" id="collapsibleNavbar">
 
-        <!-- Menu Items -->
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-        </ul>
-    </div>
+		<!-- Menu Items -->
+		<ul class="nav">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Link</a>
+			</li>
+		</ul>
+	</div>
 </nav>
--- view/home.php
+++ view/home.php
@@ -8,23 +8,23 @@
 
 
 
-    <!--Booking Search Fields Start Here-->
-    <div class="container-fluid">
+	<!--Booking Search Fields Start Here-->
+	<div class="container-fluid">
 
-        <div class="row">
-            <div class="form-group col-md-4">
-                <input type="date" class="form-control" id="date" placeholder="Date">
-            </div>
-
-
-            <div class="form-group col-md-4">
-                <input type="text" class="form-control" id="from" placeholder="From">
-            </div>
+		<div class="row">
+			<div class="form-group col-md-4">
+				<input type="date" class="form-control" id="date" placeholder="Date">
+			</div>
 
 
-            <div class="form-group col-md-4">
-                <input type="text" class="form-control" id="to" placeholder="To">
-            </div>
-        </div>
-    </div>
+			<div class="form-group col-md-4">
+				<input type="text" class="form-control" id="from" placeholder="From">
+			</div>
+
+
+			<div class="form-group col-md-4">
+				<input type="text" class="form-control" id="to" placeholder="To">
+			</div>
+		</div>
+	</div>
 </div>
