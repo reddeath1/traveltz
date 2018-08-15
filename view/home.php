46959c9c9a4b9151c1b4beac986d30d79df5b829
kaykhahima <kaykhahima@gmail.com>
2018-08-15 20:40:20 +0300

    yutcjhtutu

--- view/home.php
+++ view/home.php
@@ -6,35 +6,6 @@
 <!--The Banner starts here-->
 <div class="container-fluid hero">
 
-<nav class="navbar navbar-expand-sm">
-
-	<!-- Logo goes here -->
-	<a class="navbar-brand" href="#">LOGO HERE
-  </a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-	<div class="collapse navbar-collapse" id="collapsibleNavbar">
-
-		<!-- Menu Items -->
-		<ul class="nav">
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">Link</a>
-			</li>
-		</ul>
-	</div>
-</nav>
-
-
 
 
 	<!--Booking Search Fields Start Here-->
@@ -56,7 +27,7 @@
 		</div>
 		<div class="row">
 			<div class="form-group col-md-4 offset-md-4 text-center">
-				<button type="button" class="btn btn-dark">Check Availability</button>
+				<button type="button" class="btn btn-dark">Check Aaivailability</button>
 			</div>
 		</div>
 	</div>
