e8c9a5353ac580c12ff39c046e157b4e8584b0b1
brysonkimario <bryson.john0@gmail.com>
2018-08-18 23:06:22 +0300

    Add links to the social icons

--- footer.php
+++ footer.php
@@ -50,10 +50,9 @@
 					<p>&copy; 2018 Travel TZ Today <span class="vertical-divider">|</span> <span style="color: #6c6c6f;">Developed &amp; Maintained by <a href="http://www.khakidevelopers.com/" target="_blank" style="color: #fff">Khaki Developers</a></span></p>
 				</div>
 				<div class="col-lg-2 offset-md-4">
-					<ion-icon name="logo-instagram"></ion-icon>
-					<ion-icon name="logo-twitter"></ion-icon>
-					<ion-icon name="logo-facebook"></ion-icon>
-					<ion-icon name="logo-googleplus"></ion-icon>
+					<a href="https://www.instagram.com/traveltztoday/"><ion-icon name="logo-instagram"></ion-icon></a>
+					<a href="https://twitter.com/traveltztoday"><ion-icon name="logo-twitter"></ion-icon></a>
+					<a href="https://www.facebook.com/traveltztoday/"><ion-icon name="logo-facebook"></ion-icon></a>
 				</div>
 			</div>
 		</div>
