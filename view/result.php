8d5d560be29f31e441f6e9d1ce1bab7ac67d6794
kaykhahima <kaykhahima@gmail.com>
2018-08-21 22:01:48 +0300

    kk

--- view/result.php
+++ view/result.php
@@ -1,26 +1,5 @@
 <div class="container-fluid res">
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<form>
-					<div class="form-row">
-						<div class="input-group form-group input-group-md col-sm">
-							<div class="input-group-prepend">
-								<span class="input-group-text" id="basic-addon1"><ion-icon name="calendar"></ion-icon></span>
-							</div>
-							<input type="text" class="form-control" placeholder="Date">
-						</div>
-						<div class="form-group input-group-md col-sm">
-							<input type="text" class="form-control" placeholder="From">
-						</div>
-						<div class="form-group input-group-md col-sm">
-							<input type="text" class="form-control" placeholder="To">
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+
 
 	<div class="container">
 		<div class="row">
@@ -34,45 +13,87 @@
     <option value="3">Time</option>
     <option value="3">Seats Available</option>
   </select>
-					<select class="custom-select mb-2" id="inputGroupSelect01">
-    <option selected>Select company</option>
-    <option value="1">KLM EXP</option>
-    <option value="2">DAR EXP</option>
-  </select>
 					<div class="filters">
 						<h5>FILTERS</h5>
 						<div class="filter-border-root mb-2"></div>
 						<div class="filters-0">
 							<div class="custom-control custom-checkbox mb-2">
-							<input type="checkbox" class="custom-control-input" id="customCheck1">
-							<label class="custom-control-label" for="customCheck1">TV</label>
-						</div>
-						<div class="custom-control custom-checkbox mb-2">
-							<input type="checkbox" class="custom-control-input" id="customCheck2">
-							<label class="custom-control-label" for="customCheck2">AC</label>
-						</div>
-						<div class="custom-control custom-checkbox mb-2">
-							<input type="checkbox" class="custom-control-input" id="customCheck3">
-							<label class="custom-control-label" for="customCheck3">CHARGE</label>
-						</div>
-						<div class="custom-control custom-checkbox mb-2">
-							<input type="checkbox" class="custom-control-input" id="customCheck4">
-							<label class="custom-control-label" for="customCheck4">DRINKS</label>
-						</div>
-						<div class="custom-control custom-checkbox mb-2">
-							<input type="checkbox" class="custom-control-input" id="customCheck5">
-							<label class="custom-control-label" for="customCheck5">TOILET</label>
-						</div>
-						<div class="custom-control custom-checkbox mb-2">
-							<input type="checkbox" class="custom-control-input" id="customCheck6">
-							<label class="custom-control-label" for="customCheck6">WI-FI</label>
-						</div>
+								<input type="checkbox" class="custom-control-input" id="customCheck1">
+								<label class="custom-control-label" for="customCheck1">TV</label>
+							</div>
+							<div class="custom-control custom-checkbox mb-2">
+								<input type="checkbox" class="custom-control-input" id="customCheck2">
+								<label class="custom-control-label" for="customCheck2">AC</label>
+							</div>
+							<div class="custom-control custom-checkbox mb-2">
+								<input type="checkbox" class="custom-control-input" id="customCheck3">
+								<label class="custom-control-label" for="customCheck3">CHARGE</label>
+							</div>
+							<div class="custom-control custom-checkbox mb-2">
+								<input type="checkbox" class="custom-control-input" id="customCheck4">
+								<label class="custom-control-label" for="customCheck4">DRINKS</label>
+							</div>
+							<div class="custom-control custom-checkbox mb-2">
+								<input type="checkbox" class="custom-control-input" id="customCheck5">
+								<label class="custom-control-label" for="customCheck5">TOILET</label>
+							</div>
+							<div class="custom-control custom-checkbox mb-2">
+								<input type="checkbox" class="custom-control-input" id="customCheck6">
+								<label class="custom-control-label" for="customCheck6">WI-FI</label>
+							</div>
 						</div>
 					</div>
 
 				</div>
 			</div>
 			<div class="col-lg-10">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<form>
+								<div class="form-row">
+									<div class="input-group form-group input-group-md col-sm">
+										<div class="input-group-prepend">
+											<span class="input-group-text" id="basic-addon1"><ion-icon name="calendar"></ion-icon></span>
+										</div>
+										<input type="text" class="form-control" placeholder="Date">
+									</div>
+									<div class="form-group input-group-md col-sm">
+										<input type="text" class="form-control" placeholder="From">
+									</div>
+									<div class="form-group input-group-md col-sm">
+										<input type="text" class="form-control" placeholder="To">
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="container mb-4 accordion-0">
+					<hr>
+					<a class="mb-3 text-center" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Select Company <i class="fa fa-caret-down"></i>
+  </a>
+					<div class="collapse" id="collapseExample">
+						<div class="accordion-1">
+							<span class="badge badge-dark">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+									<label class="form-check-label" for="inlineCheckbox1">KLM EXP</label>
+								</div>
+								<span class="badge badge-light">4</span>
+							</span>
+							<span class="badge badge-dark">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+									<label class="form-check-label" for="inlineCheckbox1">KLM EXP</label>
+								</div>
+								<span class="badge badge-light">4</span>
+							</span>
+
+						</div>
+					</div>
+				</div>
 				<div class="container res-0">
 					<div class="card">
 						<div class="container">
