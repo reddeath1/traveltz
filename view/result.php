7fec3e872c4678711a749e1ea8d1c09e3d129c9f
kaykhahima <kaykhahima@gmail.com>
2018-08-21 18:14:42 +0300

    ee

--- view/result.php
+++ view/result.php
@@ -54,16 +54,16 @@
 		<div class="card">
 			<div class="container">
 				<div class="row text-center">
-					<div class="col-sm-2 image-0">
+					<div class="col-lg-2 image-0">
 						<img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16558674435%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16558674435%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.421875%22%20y%3D%22104.396875%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="rounded img-fluid" alt="...">
 						<h6 class="mt-3">KLM&nbsp;EXPRESS</h6>
 					</div>
-					<div class="col-sm-4 route">
+					<div class="col-lg-4 route">
 						<p class="mt-3">ROUTE:<strong> DAR - ARS</strong></p>
 						<p>DEPARTURE TIME: <strong>15:20Hrs</strong></p>
 						<p>ETA: <strong>6Hrs</strong></p>
 					</div>
-					<div class="col-sm-4 amenities mt-2">
+					<div class="col-lg-4 amenities mt-2">
 						<ion-icon ios="ios-wifi" md="md-wifi"></ion-icon>
 						<ion-icon name="battery-charging"></ion-icon>
 						<ion-icon name="tv"></ion-icon>
@@ -72,7 +72,7 @@
 						<p><strong>SEATS AVAILABLE:&nbsp;(27)</strong></p>
 
 					</div>
-					<div class="col-sm-2 booker">
+					<div class="col-lg-2 booker">
 						<button class="btn btn-banner">Book Now</button>
 					</div>
 				</div>
