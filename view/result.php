c8546b076eef8ecec99f81b3c2c732b1b0b494ca
kaykhahima <kaykhahima@gmail.com>
2018-08-21 20:09:18 +0300

    result page modified

--- view/result.php
+++ view/result.php
@@ -21,59 +21,114 @@
 			</div>
 		</div>
 	</div>
-	<div class="container res-0">
-		<div class="card">
-			<div class="container">
-				<div class="row text-center">
-					<div class="col-sm image-0">
-						<img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16558674435%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16558674435%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.421875%22%20y%3D%22104.396875%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="rounded img-fluid" alt="...">
-						<h6 class="mt-3">KLM&nbsp;EXPRESS</h6>
-					</div>
-					<div class="col-sm route">
-						<p class="mt-3">ROUTE:<strong> DAR - ARS</strong></p>
-						<p>DEPARTURE TIME: <strong>15:20Hrs</strong></p>
-						<p>ETA: <strong>6Hrs</strong></p>
-					</div>
-					<div class="col-sm amenities mt-2">
-						<ion-icon ios="ios-wifi" md="md-wifi"></ion-icon>
-						<ion-icon name="battery-charging"></ion-icon>
-						<ion-icon name="tv"></ion-icon>
-						<ion-icon name="cafe"></ion-icon>
-						<p class="mt-3"><strong>PRICE:&nbsp;</strong>Starts from&nbsp;<strong>30,000Tzs</strong></p>
-						<p><strong>SEATS AVAILABLE:&nbsp;(27)</strong></p>
 
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2">
+				<div class="sidebar">
+
+					<select class="custom-select mb-2" id="inputGroupSelect01">
+    <option selected>Sort by</option>
+    <option value="1">Alphabetical</option>
+    <option value="2">Price</option>
+    <option value="3">Time</option>
+    <option value="3">Seats Available</option>
+  </select>
+					<select class="custom-select mb-2" id="inputGroupSelect01">
+    <option selected>Select company</option>
+    <option value="1">KLM EXP</option>
+    <option value="2">DAR EXP</option>
+  </select>
+					<div class="filters">
+						<h5>FILTERS</h5>
+						<div class="filter-border-root mb-2"></div>
+						<div class="filters-0">
+							<div class="custom-control custom-checkbox mb-2">
+							<input type="checkbox" class="custom-control-input" id="customCheck1">
+							<label class="custom-control-label" for="customCheck1">TV</label>
+						</div>
+						<div class="custom-control custom-checkbox mb-2">
+							<input type="checkbox" class="custom-control-input" id="customCheck2">
+							<label class="custom-control-label" for="customCheck2">AC</label>
+						</div>
+						<div class="custom-control custom-checkbox mb-2">
+							<input type="checkbox" class="custom-control-input" id="customCheck3">
+							<label class="custom-control-label" for="customCheck3">CHARGE</label>
+						</div>
+						<div class="custom-control custom-checkbox mb-2">
+							<input type="checkbox" class="custom-control-input" id="customCheck4">
+							<label class="custom-control-label" for="customCheck4">DRINKS</label>
+						</div>
+						<div class="custom-control custom-checkbox mb-2">
+							<input type="checkbox" class="custom-control-input" id="customCheck5">
+							<label class="custom-control-label" for="customCheck5">TOILET</label>
+						</div>
+						<div class="custom-control custom-checkbox mb-2">
+							<input type="checkbox" class="custom-control-input" id="customCheck6">
+							<label class="custom-control-label" for="customCheck6">WI-FI</label>
+						</div>
+						</div>
 					</div>
-					<div class="col-sm align-self-center">
-						<button class="btn btn-banner">Book Now</button>
-					</div>
+
 				</div>
 			</div>
-		</div>
-	</div>
-	<div class="container res-0">
-		<div class="card">
-			<div class="container">
-				<div class="row text-center">
-					<div class="col-sm image-0">
-						<img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16558674435%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16558674435%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.421875%22%20y%3D%22104.396875%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="rounded img-fluid" alt="...">
-						<h6 class="mt-3">KLM&nbsp;EXPRESS</h6>
-					</div>
-					<div class="col-sm route">
-						<p class="mt-3">ROUTE:<strong> DAR - ARS</strong></p>
-						<p>DEPARTURE TIME: <strong>15:20Hrs</strong></p>
-						<p>ETA: <strong>6Hrs</strong></p>
-					</div>
-					<div class="col-sm amenities mt-2">
-						<ion-icon ios="ios-wifi" md="md-wifi"></ion-icon>
-						<ion-icon name="battery-charging"></ion-icon>
-						<ion-icon name="tv"></ion-icon>
-						<ion-icon name="cafe"></ion-icon>
-						<p class="mt-3"><strong>PRICE:&nbsp;</strong>Starts from&nbsp;<strong>30,000Tzs</strong></p>
-						<p><strong>SEATS AVAILABLE:&nbsp;(27)</strong></p>
+			<div class="col-lg-10">
+				<div class="container res-0">
+					<div class="card">
+						<div class="container">
+							<div class="row text-center">
+								<div class="col-sm image-0">
+									<img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16558674435%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16558674435%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.421875%22%20y%3D%22104.396875%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="rounded img-fluid" alt="...">
+									<h6 class="mt-3">KLM&nbsp;EXPRESS</h6>
+								</div>
+								<div class="col-sm route">
+									<p class="mt-3">ROUTE:<strong> DAR - ARS</strong></p>
+									<p>DEPARTURE TIME: <strong>15:20Hrs</strong></p>
+									<p>ETA: <strong>6Hrs</strong></p>
+								</div>
+								<div class="col-sm amenities mt-2">
+									<ion-icon ios="ios-wifi" md="md-wifi"></ion-icon>
+									<ion-icon name="battery-charging"></ion-icon>
+									<ion-icon name="tv"></ion-icon>
+									<ion-icon name="cafe"></ion-icon>
+									<p class="mt-3"><strong>PRICE:&nbsp;</strong>Starts from&nbsp;<strong>30,000Tzs</strong></p>
+									<p><strong>SEATS AVAILABLE:&nbsp;(27)</strong></p>
 
+								</div>
+								<div class="col-sm align-self-center">
+									<button class="btn btn-banner">Book Now</button>
+								</div>
+							</div>
+						</div>
 					</div>
-					<div class="col-sm align-self-center">
-						<button class="btn btn-banner">Book Now</button>
+				</div>
+				<div class="container res-0">
+					<div class="card">
+						<div class="container">
+							<div class="row text-center">
+								<div class="col-sm image-0">
+									<img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16558674435%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16558674435%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.421875%22%20y%3D%22104.396875%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" class="rounded img-fluid" alt="...">
+									<h6 class="mt-3">KLM&nbsp;EXPRESS</h6>
+								</div>
+								<div class="col-sm route">
+									<p class="mt-3">ROUTE:<strong> DAR - ARS</strong></p>
+									<p>DEPARTURE TIME: <strong>15:20Hrs</strong></p>
+									<p>ETA: <strong>6Hrs</strong></p>
+								</div>
+								<div class="col-sm amenities mt-2">
+									<ion-icon ios="ios-wifi" md="md-wifi"></ion-icon>
+									<ion-icon name="battery-charging"></ion-icon>
+									<ion-icon name="tv"></ion-icon>
+									<ion-icon name="cafe"></ion-icon>
+									<p class="mt-3"><strong>PRICE:&nbsp;</strong>Starts from&nbsp;<strong>30,000Tzs</strong></p>
+									<p><strong>SEATS AVAILABLE:&nbsp;(27)</strong></p>
+
+								</div>
+								<div class="col-sm align-self-center">
+									<button class="btn btn-banner">Book Now</button>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
