6453c60d8c0bc1b756ad5eeb601057485f04f1a5
kaykhahima <kaykhahima@gmail.com>
2018-08-28 17:21:24 +0300

    new page added - error page

--- view/errors/404.php
+++ view/errors/404.php
@@ -7,4 +7,29 @@
  */
 ?>
 
-ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRROR
+<div class="banner">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6 offset-lg-3 error-404">
+				<h3 class="mt-2">error</h3>
+				<h1 class="error-heading">404</h1>
+				<h6>An error occured in the application and your page could not be served.</h6>
+				<a href="?" class="btn btn-banner-outlined mt-4">BACK TO HOME</a>
+				<div class="soc">
+					<a href="https://www.instagram.com/traveltztoday/">
+						<ion-icon name="logo-instagram"></ion-icon>
+					</a>
+					<a href="https://twitter.com/traveltztoday">
+						<ion-icon name="logo-twitter"></ion-icon>
+					</a>
+					<a href="https://www.facebook.com/traveltztoday/">
+						<ion-icon name="logo-facebook"></ion-icon>
+					</a>
+					<a href="mailto:...">
+						<ion-icon name="mail"></ion-icon>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
