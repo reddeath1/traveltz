49dad82777f803a87c78a4972bffe3fdf3ea4172
kaykhahima <kaykhahima@gmail.com>
2018-08-28 14:15:36 +0300

    js now loading.

--- footer.php
+++ footer.php
@@ -70,7 +70,7 @@
             </div>
         </div>
 </footer>
-<script src="<?php echo $url;?>/view/public/js/lib/exile.js">
+	<script src="<?php echo $url;?>/view/public/js/lib/exile.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
     crossorigin="anonymous">
 </script>
